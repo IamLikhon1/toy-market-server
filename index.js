ff49c0d16849d26b15949923d53ec9fac8d8af6c
IamLikhon <likhonadnan55@gmail.com>
2023-05-18 19:11:48 +0600

    successfully add post api

--- index.js
+++ index.js
@@ -29,6 +29,20 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+        const toysCollection=client.db('masterToy').collection('toys');
+
+
+    app.post('/addToy',async(req,res)=>{
+        const toyData=req.body;
+        const result=await toysCollection.insertOne(toyData);
+        res.send(result)
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
