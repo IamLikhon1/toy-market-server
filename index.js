f1a8bc3c42983d00ef15af8b9e6b1f017bd992da
IamLikhon <likhonadnan55@gmail.com>
2023-05-18 23:04:55 +0600

    added the query email

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const port=process.env.PORT||5000;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app=express();
 
 // Middleware:
@@ -33,9 +33,26 @@ async function run() {
         const toysCollection=client.db('masterToy').collection('toys');
 
     app.get('/allToy',async(req,res)=>{
-        const result=await toysCollection.find().toArray();
+        let query={}
+        if(req.query?.sellerEmail){
+            query={sellerEmail:req.query.sellerEmail}
+        }
+        
+        const result=await toysCollection.find(query).toArray();
         res.send(result)
-    })
+    });
+
+    app.get('/allToy/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)};
+        const result=await toysCollection.findOne(query);
+        res.send(result)
+    });
+
+    // app.get('/allToy',async(req,res)=>{
+    //     const result=await toysCollection.find().toArray();
+    //     res.send(result)
+    // })
 
 
     app.post('/addToy',async(req,res)=>{
